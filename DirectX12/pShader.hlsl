f42f7773aea9aa85b4b1763497fa76e5a1999553
Christopher Wiedeman <97039271+cwiedeman-fullsail@users.noreply.github.com>
2022-11-09 13:05:30 -0500

    Add files via upload

---
+++ DirectX12/pShader.hlsl
@@ -0,0 +1,4 @@
+float4 main() : SV_TARGET
+{
+    return float4(0.5f, 0.25f, 0.75f, 0); // TODO: Part 1a
+}
---
+++ DirectX12/vShader.hlsl
@@ -0,0 +1,5 @@
+float4 main(float2 inputVertex : POSITION) : SV_POSITION
+{
+	// TODO: Part 2d, Part 2f, Part 3b
+    return float4(inputVertex, 0, 1);
+}
